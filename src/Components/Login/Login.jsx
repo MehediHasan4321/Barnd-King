1814e4c423902a7e744f7b3ce5d6a6804ca8d198
MehediHasan4321 <126mehedi@gmail.com>
2023-04-16 16:40:46 +0600

    updete login from and also install dasiUi

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,25 +5,27 @@ import app from '../../Firebase/Firebase.comfig';
 const Login = () => {
     const [error, setError] = useState('')
     const [success, setSuccess] = useState('')
-    const [type,setType] = useState('password')
+    const [type, setType] = useState('password')
     const emailRef = useRef()
     const auth = getAuth(app);
-
-    const handelLogIn = e => {
-        e.preventDefault()
+    setTimeout(() => {
         setError('')
         setSuccess('')
+    }, 7000)
+    const handelLogIn = e => {
+        e.preventDefault()
         const email = e.target.email.value;
         const password = e.target.password.value
         signInWithEmailAndPassword(auth, email, password)
             .then(() => {
                 setSuccess('Log In successful')
+                e.target.reset()
             })
             .catch(error => {
                 setError(error.message)
             })
 
-        e.target.reset()
+        
     }
 
     const forgetPassword = e => {
@@ -46,44 +48,60 @@ const Login = () => {
                 setError(error.message)
             })
     }
-    const handleType = ()=>{
-        if(type==='text'){
+    const handleType = () => {
+        if (type === 'text') {
             setType('password')
         }
-        else if(type === 'password'){
+        else if (type === 'password') {
             setType('text')
         }
     }
     return (
-        <div className='w-full h-[80vh] flex items-center justify-center '>
-            <div className='w-96 h-auto py-12 shadow-xl px-4 space-y-4'>
-                <form onSubmit={handelLogIn} className='space-y-4'>
-                    <input className='w-full px-4 py-2 text-left outline-none rounded-md bg-gray-400 text-white text-lg font-semibold placeholder:text-white' placeholder='Enter Your Email' ref={emailRef} type="email" name="email" id="email" />
-                    <div className=' relative text-white'>
-                    <input className='w-full px-4 py-2 text-left outline-none rounded-md bg-gray-400 text-white text-lg font-semibold placeholder:text-white' placeholder='Enter Your Password' type={type} name="password" id="password" />
-                    <span onClick={handleType} className=' absolute right-2 top-3'>{type==='password' ? <i class="fa-solid fa-eye"></i>:<i class="fa-solid fa-eye-slash"></i>}</span>
-                    </div>
-                    <input className='w-full rounded-md px-4 py-2 bg-purple-400 text-white text-lg font-semibold' type="submit" name="submit" id="submit" value={'Log In'} />
-                </form>
-
-                <div>
-                    {success && <p className='text-green-400 text-center'><small>{success}</small></p>}
-                    {error && <p className='text-red-500 text-center'><small>{error}</small></p>}
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
                 </div>
-                <div className=' flex gap-4 items-center'>
-                    <p>Also Log In With ..... </p>
-                    <span onClick={singInWithGoogle} className='text-purple-400 hover:text-purple-600 cursor-pointer text-2xl'>
-                        <i class="fa-brands fa-google-plus-g"></i>
-                    </span>
-                    <span className='text-purple-400 cursor-pointer hover:text-purple-600 text-2xl'>
-                        <i class="fa-brands fa-square-facebook"></i>
-                    </span>
-                    <span className='text-purple-400 cursor-pointer hover:text-purple-600 text-2xl'>
-                        <i class="fa-brands fa-github"></i>
-                    </span>
-                </div>
-                <div className='mt-4'>
-                    <p>Don't Hava An Account? Or <Link to={'/signUp'}><span className=' text-purple-400'>Sign Up</span></Link> Or <span onClick={forgetPassword} className='text-purple-400 cursor-pointer'>Forget Password</span></p>
+                <div className="card flex-shrink-0 w-96 max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handelLogIn} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" required name='email' placeholder="example@gmail.com" className="input input-bordered" />
+                        </div>
+                        <div className="form-control relative">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type={type} required name='password' placeholder="Enter Your Password" className="input input-bordered outline-none" />
+                            <span onClick={handleType} className=' absolute right-2 top-12'>{type==='password' ? <i class="fa-solid fa-eye"></i>:<i class="fa-solid fa-eye-slash"></i>}</span>
+                            <label className="label">
+                                <p onClick={forgetPassword} className="label-text-alt text-purple-400 link link-hover">Forgot password?</p>
+                            </label>
+                        </div>
+                        <div>
+                            {success && <p className='text-green-400 text-center'><small>{success}</small></p>}
+                            {error && <p className='text-red-500 text-center'><small>{error}</small></p>}
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className=" py-2 bg-purple-400 outline-none text-lg text-white rounded-xl">Login</button>
+                        </div>
+                        <div className=' flex justify-center gap-4 items-center'>
+                            <span onClick={singInWithGoogle} title='Sign in with Google' className='text-purple-400 hover:text-purple-600 cursor-pointer text-2xl'>
+                                <i class="fa-brands fa-google-plus-g"></i>
+                            </span>
+                            <span className='text-purple-400 cursor-pointer hover:text-purple-600 text-2xl' title='Sign in with Facebook'>
+                                <i class="fa-brands fa-square-facebook"></i>
+                            </span>
+                            <span className='text-purple-400 cursor-pointer hover:text-purple-600 text-2xl' title='Sign in with Github'>
+                                <i class="fa-brands fa-github"></i>
+                            </span>
+                        </div>
+                        <label className="label">
+                            <Link to={'/signUp'} className="label-text-alt text-purple-400 link link-hover">Don't have an account? Sign Up</Link>
+                        </label>
+                    </form>
                 </div>
             </div>
         </div>
